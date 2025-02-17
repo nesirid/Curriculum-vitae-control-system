e19175d6acc043cedb5415a4b2c259890e166f28
nesirid <nasirid@code.edu.az>
2025-02-17 16:55:31 +0400

    First commit create clear structure for application CV control system

---
+++ App/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Repository.Data;
+using App.Middlewares;
+using Microsoft.OpenApi.Models;
+using Service;
+using App.Configurations;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Logging configuration
+AppConfiguration.ConfigureLogging(builder);
+
+// Registration of services
+AppConfiguration.ConfigureServices(builder);
+
+var app = builder.Build();
+
+// Setting up Middleware and endpoints
+AppConfiguration.ConfigureMiddleware(app);
+
+// Launching the application
+app.Run();
---
+++ Service/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Service
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddServiceLayer(this IServiceCollection services)
+        {
+            // Automatic mapping profiling
+            //services.AddAutoMapper(typeof(MappingProfile).Assembly);
+
+
+            // Registration of validators (if any)
+            //services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+
+            return services;
+        }
+    }
+}
